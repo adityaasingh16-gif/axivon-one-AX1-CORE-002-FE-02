07f605081bb54d68af8b7061298f947f48d205b7
adityaasingh16-gif <adityaasingh162007@gmail.com>
2026-09-22 15:11:46 +0530

    feat(file-management): expose reusable frontend components

---
+++ modules/core/file-management/frontend/index.ts
@@ -0,0 +1,2 @@
+export { FileCard, FileManagementScreen, FilePreviewPanel, FileUploadPanel } from '../../../../apps/frontend/src/modules/core/file-management/FileManagementScreen';
+export type { FileManagementScreenProps } from '../../../../apps/frontend/src/modules/core/file-management/FileManagementScreen';
